27814650c81f5880d5babd45bc02f3fee4f71140
ollvilhi <ollvilhi@jyu.fi>
2025-07-03 23:29:09 +0300

    Pohja

---
+++ taksopeli.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Koodi suoritetaan, kun sivu on ladattu
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript on valmis ja strict-tila on käytössä!");
+});
